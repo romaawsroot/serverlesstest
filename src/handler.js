416e4d6d89d542e67da061185c00f6bbd5acf7c9
Roman Qarayev <rqarayev@azercell.com>
2020-11-04 12:05:10 +0400

    dd

--- src/handler.js
+++ src/handler.js
@@ -1,1 +1,13 @@
-'use strict';  module.exports.hello = async event => {   return {     statusCode: 200,     body: JSON.stringify(       {         message: 'Your function executed successfully!'       },       null,       2     ),   }; };
+'use strict';  
+module.exports.hello = async event => {   
+	return {    
+          statusCode: 200,     
+          body: JSON.stringify(      
+		  {         
+	           message: 'Your function executed successfully!'       
+		  },       
+		  null,     
+		  2     
+	          ),  
+	        }; 
+                      };
